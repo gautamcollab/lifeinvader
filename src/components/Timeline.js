36b8fe98534342882ade4d2506957733ffb931fa
Gautam Sinha <gautam.sinha@live.com>
2022-04-08 13:10:10 -0400

    data

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -4,11 +4,11 @@ const Timeline = () => {
     const [posts, setPosts] = useState([])
 
     useEffect(async () => {
-        console.log('res')
+        
         let req = await fetch('https://github.com/rmdashrfv/lifeinvader/blob/main/src/data/posts.json')
         let res = await req.json()
         setPosts(res)
-    }, [])
+    })
 
     return (
         <div>
