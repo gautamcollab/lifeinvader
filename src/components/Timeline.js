d5c09818c9c7cd02dd05a89ec3c40c717fa5886e
Gautam Sinha <gautam.sinha@live.com>
2022-04-08 13:25:16 -0400

    added posts to timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -3,17 +3,28 @@ import { useEffect, useState } from 'react';
 const Timeline = () => {
     const [posts, setPosts] = useState([])
 
-    useEffect(async () => {
-        
-        let req = await fetch('https://github.com/rmdashrfv/lifeinvader/blob/main/src/data/posts.json')
-        let res = await req.json()
-        setPosts(res)
-    })
+    useEffect(() => {
+        // IIFE: immediately invoked function expression
+        (async () => {
+          let req = await fetch('https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json')
+          let res = await req.json()
+          setPosts(res)
+        })()
+      }, [])
 
     return (
         <div>
             <h2>Timeline</h2>
-        
+        {
+            posts.map((element) => {
+                return(
+                    <div>
+                       <p>Post by {element.username}</p> 
+                       <p>{element.content}</p>
+                    </div> 
+                )
+            })
+        }
         </div> 
     )
 }
