67f0528133249b84987734d6aea85332823c1ca2
Gautam Sinha <gautam.sinha@live.com>
2022-04-08 14:34:05 -0400

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+
+const Navbar = () => {
+    return(
+        <nav className="navbar">
+            <div>
+                <a href="#">lifeinvader</a>
+            </div>
+            <div>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <input type="search" placeholder="Search"/>
+            </div>
+            <div>
+            <i class="fa-solid fa-bell"></i>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar; 
